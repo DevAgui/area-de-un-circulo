f4a80f7805421d13fffd4749619635fdc49bcb16
DevAgui <dagmartin98@gmail.com>
2022-10-05 13:32:50 +0200

    Agregar archivos de proyecto.

---
+++ Circulo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Circulo
+{
+    internal class Program
+    {
+        static void Main(string[] args){
+
+            Console.WriteLine("Éste programa te dice el área de un círculo");
+            
+            Console.WriteLine("Introduce el primer número");
+            double radio = int.Parse(Console.ReadLine());
+
+            const double PI = 3.14;
+
+            double resultado = PI * Math.Pow(radio, 2);
+
+            Console.WriteLine("El área del círculo de radio {0} es de: {1}", radio, resultado);
+
+
+        }
+    }
+}
